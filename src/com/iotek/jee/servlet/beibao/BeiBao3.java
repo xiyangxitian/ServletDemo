53b0311305cf9d336ae1ac6ee4db2e4f056402f3
liyan <liyan@e-ling.com.cn>
2019-04-06 19:02:56 +0800

    取所有的组合列

--- src/com/iotek/jee/servlet/beibao/BeiBao3.java
+++ src/com/iotek/jee/servlet/beibao/BeiBao3.java
@@ -1,9 +1,24 @@
 package com.iotek.jee.servlet.beibao;
 
 public class BeiBao3 {
+    //从m个数中取n个数共有多少个可能的组合。
+    static int combin(int m, int n) {
+        if (m == n || n == 0) {
+            System.out.println(1);
+            return 1;
+        } else {
+            int b = combin(m - 1, n) + combin(m - 1, n - 1);
+            System.out.println(b);
+            return b;
+        }
+    }
 
-
-
-
+    //1,2,3  1,2  1,3 2,3
+    //1,2,3,4  1,2,3, 1,2,4 1,3,4 2,3,4
+    //1,2,3,4  1,2  1,3 1,4 2,3 2,4 3,4
+    public static void main(String[] args) {
+        int combin = combin(4, 2);
+        System.out.println("结果:" + combin);
+    }
 
 }
--- src/test/Test1.java
+++ src/test/Test1.java
@@ -3,6 +3,7 @@ package test;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Test1 {
@@ -13,6 +14,38 @@ public class Test1 {
         System.out.println(list.add(0));
     }
 
+    @Test
+    public void test2(){
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(0);
+        list.add(0);
+        list.add(0);
+        list.set(2, 3);
+        System.out.println(Arrays.toString(list.toArray()));
+    }
+    @Test
+    public void test3(){
+        System.out.printf("%d ", 1);
+        System.out.printf("%d ", 2);
+        System.out.printf("%d ", 3);
+    }
+    @Test
+    public void test4(){
+
+    }
+    @Test
+    public void test5(){
+
+    }
+    @Test
+    public void test6(){
+
+    }
+    @Test
+    public void test7(){
+
+    }
 
 
 
